bd3be960bb336780dacae5f7e6705bed422c7d10
tumon <tumonkhn@gmail.com>
2022-11-03 01:15:29 +0600

    init

--- components/showPod/ShowPod.tsx
+++ components/showPod/ShowPod.tsx
@@ -1,8 +1,36 @@
 "use client";
-interface Props {
-  data: object;
+
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./ShowPod.module.scss";
+
+interface RssTypes {
+  data: {
+    title: string;
+    description: string;
+    link: string;
+    image: string;
+    category: [];
+    items: [];
+  };
 }
-export default function ShowPod({ data }: Props) {
+
+export default function ShowPod({ data }: RssTypes) {
   console.log(data);
-  return <div> Show Pod</div>;
+  return (
+    <main className={styles.main}>
+      <div className={styles.mainHead}>
+        <Image
+          src={data.image}
+          className={styles.mainImg}
+          height="40"
+          width="40"
+          alt="img"
+        />
+        <Link href={data.link}>
+          <h3>{data.title}</h3>
+        </Link>
+      </div>
+    </main>
+  );
 }
--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: { appDir: true },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.buzzsprout.com",
+        port: "",
+        pathname: "/variants/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
