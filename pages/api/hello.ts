b9803629642fa99b91d7b9e76a061f521df1cf0e
tumon <tumonkhn@gmail.com>
2022-11-02 16:35:59 +0600

    init

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,39 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+const { parse } = require("rss-to-json");
 
 type Data = {
-  name: string
-}
+  name: string;
+  data: any;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      try {
+        var rss = await parse("https://changelog.com/jsparty/feed");
+
+        if (rss) res.status(200).json({ name: "John Doe", data: rss });
+      } catch (err) {
+        res.status(400).json({ name: "John Doe", data: "error" });
+      }
+      break;
+
+    // case "POST":
+    //   try{
+    //       const data = await users.create(req.body);
+    //       res.status(201).json({ success: true, data: data })
+    //   } catch(err){
+    //       res.status(400).json({ success: false })
+    //   }
+    //   break;
+
+    default:
+      res.status(200).json({ name: "John Doe", data: "default error" });
+      break;
+  }
 }
