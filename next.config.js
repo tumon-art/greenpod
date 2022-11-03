82c9b4889fd0f69bc848a7564f53c69507cb0490
tumon <tumonkhn@gmail.com>
2022-11-03 20:34:31 +0600

    init

--- next.config.js
+++ next.config.js
@@ -2,17 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: { appDir: true },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "storage.buzzsprout.com",
-        port: "",
-        pathname: "/variants/**",
-      },
-    ],
-  },
+  // experimental: { appDir: true },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "storage.buzzsprout.com",
+  //       port: "",
+  //       pathname: "/variants/**",
+  //     },
+  //   ],
+  // },
 };
 
 module.exports = nextConfig;
---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import styles from "../styles/Home.module.scss";
+// import ShowPod from "../components/ShowPod";
+
+// async function getData() {
+//   const res = await fetch("http://localhost:3000/api/hello");
+//   return res.json();
+// }
+
+export default async function Home() {
+  // const { data } = await getData();
+  return (
+    <main>
+      {/* <ShowPod /> */}
+      <h1> Hell o</h1>
+    </main>
+  );
+}
