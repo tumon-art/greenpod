1f4f1c214a4aa3c4c625aecee90e1f18591883e4
tumon <tumonkhn@gmail.com>
2022-11-03 18:01:10 +0600

    init

--- app/ShowPod.tsx
+++ app/ShowPod.tsx
@@ -6,93 +6,101 @@ import { useEffect, useRef, useState } from "react";
 import styles from "../styles/ShowPod.module.scss";
 
 interface RssTypes {
-  data: {
-    title: string;
-    description: string;
-    link: string;
-    image: string;
-    category: [];
-    items: [
-      {
-        title: string;
-        description: string;
-        published: string;
-        created: string;
-        category: [];
-        content: string;
-        enclosures: [{ lenght: string; type: string; url: string }];
-      }
-    ];
-  };
+  title: string;
+  description: string;
+  link: string;
+  image: string;
+  category: [];
+  items: [
+    {
+      title: string;
+      description: string;
+      published: string;
+      created: string;
+      category: [];
+      content: string;
+      enclosures: [{ lenght: string; type: string; url: string }];
+    }
+  ];
 }
 
 export default function ShowPod() {
   const [isPlaying, setisPlaying] = useState<boolean>(false);
-
+  const [duration, setDuration] = useState<number>(0);
   const [data, setData] = useState<RssTypes | null>(null);
   const [isLoading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setLoading(true);
-    fetch("https://greenpod-five.vercel.app/api/hello")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      });
-  }, []);
-
-  if (isLoading) return <p>Loading...</p>;
-  if (!data) return <p>No profile data</p>;
   const audioPlayer = useRef<HTMLAudioElement>(null);
 
   const togglePlayPause = () => {
     setisPlaying((p) => !p);
 
-    if (isPlaying) {
+    if (!isPlaying) {
       audioPlayer.current?.play();
     } else audioPlayer.current?.pause();
   };
 
-  // return (
-  //   <main>
-  //     <span> shello </span>
-  //   </main>
-  // );
+  useEffect(() => {
+    setLoading(true);
+    fetch("http://localhost:3000/api/hello")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.data);
+        setLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (audioPlayer.current) {
+      console.log(audioPlayer.current.duration);
+      // progressBar.current.max = seconds;
+    }
+  }, [audioPlayer.current?.onloadedmetadata, audioPlayer.current?.readyState]);
+
+  const calculateTime = (secs: any) => {
+    const minutes = Math.floor(secs / 60);
+    const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${returnedMinutes}:${returnedSeconds}`;
+  };
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No profile data</p>;
+
   return (
     <main className={styles.main}>
       <div className={styles.mainHead}>
         <Image
-          src={data.data.image}
+          src={data.image}
           className={styles.mainImg}
           height="40"
           width="40"
           alt="img"
         />
-        <Link href={data.data.link}>
-          <h3>{data.data.title}</h3>
+        <Link href={data.link}>
+          <h3>{data.title}</h3>
         </Link>
       </div>
 
       {/* === AUDIO PLAYER === */}
-      <audio
-        ref={audioPlayer}
-        src={data.data.items[0].enclosures[0].url}
-        preload="metadata"
-      ></audio>
 
       <div>
-        <audio controls>
-          <source
-            src={data.data.items[0].enclosures[0].url}
-            type="audio/mpeg"
-          />
-          Your browser does not support the audio element.
-        </audio>
+        <audio
+          ref={audioPlayer}
+          src={data.items[0].enclosures[0].url}
+          onLoadedMetadata={() => {
+            if (audioPlayer.current) {
+              const seconds = Math.floor(audioPlayer.current!.duration);
+              setDuration(seconds);
+            }
+          }}
+          preload="metadata"
+        ></audio>
       </div>
 
       {/* <button>30sec</button> */}
-      {/* <button onClick={togglePlayPause}>{isPlaying ? "pause" : "play"}</button> */}
+      <button onClick={togglePlayPause}>{isPlaying ? "pause" : "play"}</button>
       {/* <button>30sec</button> */}
       {/* CURRENT TIME  */}
       {/* <div> 0:00</div> */}
@@ -101,7 +109,7 @@ export default function ShowPod() {
       {/* <input type="range" /> */}
       {/* </div> */}
       {/* DURATION */}
-      {/* <div>3:00</div> */}
+      <div>{calculateTime(duration)}</div>
     </main>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import styles from "../styles/Home.module.scss";
 import ShowPod from "./ShowPod";
 
-export default function Page() {
+// async function getData() {
+//   const res = await fetch("http://localhost:3000/api/hello");
+//   return res.json();
+// }
+
+export default async function Page() {
+  // const { data } = await getData();
   return (
     <main>
       <ShowPod />
