99b253545038d7f31e646d3d72c65a29f58e3a78
tumon <tumonkhn@gmail.com>
2022-11-06 13:13:05 +0600

    init

---
+++ app/onepath/page.tsx
@@ -0,0 +1,15 @@
+import ShowPod from "../../components/ShowPod";
+
+async function getData() {
+  const res = await fetch("https://greenpod.fly.dev/onepath");
+  return res.json();
+}
+
+export default async function page() {
+  const { data } = await getData();
+  return (
+    <div>
+      <ShowPod data={data} />
+    </div>
+  );
+}
