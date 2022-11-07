b803d7f409ac952b7b87924810a7ec210a7bde57
tumon <tumonkhn@gmail.com>
2022-11-07 10:09:54 +0600

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Inter } from "@next/font/google";
-import Header from "../components/Header";
+import Header from "./Header";
 
 import "../styles/globals.scss";
 
@@ -13,6 +13,7 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang="en" className={inter.className}>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Create Next App</title>
         <meta name="description" content="GreenPod" />
         <link rel="icon" href="/favicon.ico" />
